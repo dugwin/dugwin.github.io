036d5a3d4cdec30e8eb257df2f36bc606e6c5e5c
verhovcev <roman@verhovcev.net>
2016-09-03 00:21:21 +0300

    "update"

--- script/dynamic.js
+++ script/dynamic.js
@@ -4,6 +4,7 @@
     if (hst == '127.' || hst == '192.' || document.location.hostname.toLowerCase() == 'localhost') {
         fieldNameElement.innerHTML += 'running local</br>';
         fieldNameElement.innerHTML += '<a href="/cgi/edit/">new post</a><br/>';
+        fieldNameElement.innerHTML += '<a href="/cgi/gitchanges">git changes</a><br/>';
         fieldNameElement.innerHTML += '<a href="/cgi/s3/">s3 console</a>';
         
         var posts = document.getElementsByName('dynamicdiv');
